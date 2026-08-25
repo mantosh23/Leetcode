edc9fbb78dacb2569e6da71476c3c6607f3fb118
Mantosh Kumar <62930889+mantosh23@users.noreply.github.com>
2026-08-25 17:33:32 +0530

    Time: 1325 ms (5.02%) | Memory: 52.4 MB (93.52%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -14,37 +14,43 @@ public class Solution {
         ListNode l1 = headA;
         ListNode l2 = headB;
 
-        int sizeA = 0;
-        int sizeB = 0;
-
-        while(l1 != null){
-            sizeA++;
-            l1 = l1.next;
-        }
-
-        while(l2 != null){
-            sizeB++;
-            l2 = l2.next;
-        }
-
-        l1 = headA;
-        l2 = headB;
-
-        while(sizeA > sizeB){
-            l1 = l1.next;
-            sizeA--;
-        } 
-
-        while(sizeB > sizeA){
-            l2 = l2.next;
-            sizeB--;
-        } 
-
         while(l1 != l2){
-            l1 = l1.next;
-            l2 = l2.next;
+            l1 = l1 == null ? headA : l1.next;
+            l2 = l2 == null ? headB : l2.next;
         }
-
         return l1;
+
+        // int sizeA = 0;
+        // int sizeB = 0;
+
+        // while(l1 != null){
+        //     sizeA++;
+        //     l1 = l1.next;
+        // }
+
+        // while(l2 != null){
+        //     sizeB++;
+        //     l2 = l2.next;
+        // }
+
+        // l1 = headA;
+        // l2 = headB;
+
+        // while(sizeA > sizeB){
+        //     l1 = l1.next;
+        //     sizeA--;
+        // } 
+
+        // while(sizeB > sizeA){
+        //     l2 = l2.next;
+        //     sizeB--;
+        // } 
+
+        // while(l1 != l2){
+        //     l1 = l1.next;
+        //     l2 = l2.next;
+        // }
+
+        // return l1;
     }
 }
